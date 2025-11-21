f01e2ac4e16738bfc30472d4dca7000f2bdc4717
Nosenchuck <pantingpug@yahoo.com>
2025-11-21 14:03:18 -0500

    UI design changes

--- src/components/PageLayout.jsx
+++ src/components/PageLayout.jsx
@@ -1,40 +1,39 @@
 /*
- * Copyright (c) Microsoft Corporation. All rights reserved.
- * Licensed under the MIT License.
+ * App shell: top navbar + content container
  */
 
-import React from 'react';
-import Navbar from 'react-bootstrap/Navbar';
+import React from "react";
+import Navbar from "react-bootstrap/Navbar";
+import { useIsAuthenticated } from "@azure/msal-react";
+import { SignInButton } from "./SignInButton";
+import { SignOutButton } from "./SignOutButton";
 
-import { useIsAuthenticated } from '@azure/msal-react';
-import { SignInButton } from './SignInButton';
-import { SignOutButton } from './SignOutButton';
-
-/**
- * Renders the navbar component with a sign-in or sign-out button depending on whether or not a user is authenticated
- * @param props
- */
 export const PageLayout = (props) => {
-    const isAuthenticated = useIsAuthenticated();
+  const isAuthenticated = useIsAuthenticated();
 
-    return (
-        <>
-            <Navbar bg="primary" variant="dark" className="navbarStyle">
-                <a className="navbar-brand" href="/">
-                    Microsoft Identity Platform
-                </a>
-                <div className="collapse navbar-collapse justify-content-end">
-                    {isAuthenticated ? <SignOutButton /> : <SignInButton />}
-                </div>
-            </Navbar>
-                <div className="title">
-                    <h5>
-                        Welcome to the Microsoft Authentication Library For JavaScript - React SPA
-                    </h5>
-                </div>
-                <div className="profileContent">
-                    {props.children}
-                </div>
-        </>
-    );
+  return (
+    <>
+      <Navbar className="navbarStyle" expand="md">
+        <div className="navbar-inner">
+          <div className="navbar-left">
+            <span className="navbar-logo">▢</span>
+            <span className="navbar-brand-text">Email Filter</span>
+          </div>
+
+          <nav className="navbar-links">
+            <span className="navbar-link">Home</span>
+            <span className="navbar-link">How it works</span>
+            <span className="navbar-link">Dashboard</span>
+          </nav>
+
+          <div className="navbar-actions">
+            {isAuthenticated ? <SignOutButton /> : <SignInButton />}
+          </div>
+        </div>
+      </Navbar>
+
+      <main className="app-shell">{props.children}</main>
+    </>
+  );
 };
+
